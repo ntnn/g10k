8a36b172e07491cce0cffe6543ba9a96b497284f
Andreas Paul <andreas.paul@1und1.de>
2015-11-10 17:38:46 +0100

    fix Puppetfile parsing

--- config.go
+++ config.go
@@ -74,11 +74,7 @@ func readPuppetfile(targetDir string, sshKey string) Puppetfile {
 		for scanner.Scan() {
 			line := scanner.Text()
 			if !reComment.MatchString(line) && !reEmpty.MatchString(line) {
-				if regexp.MustCompile(",\\s*$").MatchString(line) {
-					n += line
-				} else {
-					n += line + "\n"
-				}
+				n += line + "\n"
 			}
 		}
 		if err := scanner.Err(); err != nil {
@@ -86,98 +82,96 @@ func readPuppetfile(targetDir string, sshKey string) Puppetfile {
 		}
 
 		reModuledir := regexp.MustCompile("^\\s*(?:moduledir)\\s*['\"]?([^'\"]+)['\"]")
-		reForgeModule := regexp.MustCompile("^\\s*(?:mod)\\s*['\"]?([^'\"]+/[^'\"]+)['\"](?:\\s*(,)\\s*['\"]?([^'\"]*))?")
-		reGitModule := regexp.MustCompile("^\\s*(?:mod)\\s*['\"]?([^'\"/]+)['\"]\\s*,(.*)")
-		reGitAttribute := regexp.MustCompile("\\s*:(git|commit|tag|branch|ref)\\s*=>\\s*['\"]?([^'\"]+)['\"]")
+		reModuleName := regexp.MustCompile("^\\s*(?:mod)\\s*['\"]?([^'\"]+)['\"]?(?:\\s*(,)\\s*['\"]?([^'\"]*))?")
+		reModuleAttribute := regexp.MustCompile("\\s*:(git|commit|tag|branch|ref|version)\\s*=>\\s*['\"]?([^'\"]+)['\"]")
 		//moduleName := ""
 		//nextLineAttr := false
 
 		for _, line := range strings.Split(n, "\n") {
-			//fmt.Println("found line ---> ", line)
+			fmt.Println("found line ---> ", line)
 			if strings.Count(line, ":git") > 1 || strings.Count(line, ":tag") > 1 || strings.Count(line, ":branch") > 1 || strings.Count(line, ":ref") > 1 {
 				log.Fatal("Error: trailing comma found in", pf, "somewhere here: ", line)
 				os.Exit(1)
 			}
 			if m := reModuledir.FindStringSubmatch(line); len(m) > 1 {
 				puppetFile.moduleDir = m[1]
-			} else if m := reForgeModule.FindStringSubmatch(line); len(m) > 1 {
-				//fmt.Println("found forge mod name ---> ", m[1])
-				comp := strings.Split(m[1], "/")
-				if len(comp) != 2 {
-					log.Print("Forge module name is invalid, should be like puppetlabs/apt, but is:", m[3], "in", pf, "line: ", line)
-					os.Exit(1)
+			} else if m := reModuleName.FindStringSubmatch(line); len(m) > 1 {
+				fmt.Println("found mod name ---> ", m[1])
+				comp := m[1]
+				if strings.Contains(m[1], "/") {
+					comp = strings.Split(m[1], "/")[1]
 				}
+				fmt.Println("found comp ---> ", comp)
+
+				// TODO fail if modulename != (string[/_-])?string
+
 				if _, ok := puppetFile.forgeModules[m[1]]; ok {
 					log.Fatal("Error: Duplicate forge module found in ", pf, " for module ", m[1], " line: ", line)
 					os.Exit(1)
 				}
-				if len(m[3]) > 1 {
-					if m[3] == ":latest" {
-						puppetFile.forgeModules[m[1]] = ForgeModule{version: "latest", name: comp[1], author: comp[0]}
-					} else {
-						puppetFile.forgeModules[m[1]] = ForgeModule{version: m[3], name: comp[1], author: comp[0]}
-					}
-					//fmt.Println("found m[1] ---> '", m[1], "'")
-					//fmt.Println("found forge mod attribute ---> ", m[3])
-				} else {
-					//puppetFile.forgeModules[m[1]] = ForgeModule{}
-					puppetFile.forgeModules[m[1]] = ForgeModule{version: "present", name: comp[1], author: comp[0]}
-				}
-			} else if m := reGitModule.FindStringSubmatch(line); len(m) > 1 {
-				//fmt.Println("found git mod name ---> ", m[1])
-				if strings.Contains(m[1], "-") {
-					fmt.Println("Warning: Found invalid character '-' in Puppet module name", m[1], "in", pf, "line:", line)
-				}
-				if len(m[2]) > 1 {
-					gitModuleAttributes := m[2]
-					if strings.Count(gitModuleAttributes, ":git") < 1 {
-						log.Fatal("Error: Missing :git url in ", pf, " for module ", m[1], " line: ", line)
-						os.Exit(1)
-					}
-					if strings.Count(gitModuleAttributes, ",") > 2 {
-						log.Fatal("Error: Too many attributes in ", pf, " for module ", m[1], " line: ", line)
-						os.Exit(1)
-					}
-					if _, ok := puppetFile.gitModules[m[1]]; ok {
-						log.Fatal("Error: Duplicate module found in ", pf, " for module ", m[1], " line: ", line)
-						os.Exit(1)
-					}
-					puppetFile.gitModules[m[1]] = GitModule{}
-					//fmt.Println("found git mod attribute ---> ", gitModuleAttributes)
-					if a := reGitAttribute.FindStringSubmatch(gitModuleAttributes); len(a) > 1 {
-						gm := GitModule{}
-						//fmt.Println("found for git mod ", m[1], " attribute ", a[1], " with value ", a[2])
-						if a[1] == "git" {
-							gm.git = a[2]
-						} else if a[1] == "branch" {
-							gm.branch = a[2]
-						} else if a[1] == "tag" {
-							gm.tag = a[2]
-						} else if a[1] == "commit" {
-							gm.commit = a[2]
-						} else if a[1] == "ref" {
-							gm.ref = a[2]
-						}
-						if strings.Contains(gitModuleAttributes, ",") {
-							if a := reGitAttribute.FindStringSubmatch(strings.SplitN(gitModuleAttributes, ",", 2)[1]); len(a) > 1 {
-								if a[1] == "git" {
-									gm.git = a[2]
-								} else if a[1] == "branch" {
-									gm.branch = a[2]
-								} else if a[1] == "tag" {
-									gm.tag = a[2]
-								} else if a[1] == "commit" {
-									gm.commit = a[2]
-								} else if a[1] == "ref" {
-									gm.ref = a[2]
-								}
-								//puppetFile.gitModules[m[1]] = GitModule{a[1]: a[2]}
-								//fmt.Println("found for git mod ", m[1], " attribute ", a[1], " with value ", a[2])
-							}
+				//if len(m[3]) > 1 {
+				//	if m[3] == ":latest" {
+				//		puppetFile.forgeModules[m[1]] = ForgeModule{version: "latest", name: comp[1], author: comp[0]}
+				//	} else {
+				//		puppetFile.forgeModules[m[1]] = ForgeModule{version: m[3], name: comp[1], author: comp[0]}
+				//	}
+				//	//fmt.Println("found m[1] ---> '", m[1], "'")
+				//	//fmt.Println("found forge mod attribute ---> ", m[3])
+				//} else {
+				//	//puppetFile.forgeModules[m[1]] = ForgeModule{}
+				//	puppetFile.forgeModules[m[1]] = ForgeModule{version: "present", name: comp[1], author: comp[0]}
+				//}
+				if m := reModuleAttribute.FindStringSubmatch(line); len(m) > 1 {
+					fmt.Println("found attribute ---> '", m[1], "'")
+					//	gitModuleAttributes := m[2]
+					//	if strings.Count(gitModuleAttributes, ":git") < 1 {
+					//		log.Fatal("Error: Missing :git url in ", pf, " for module ", m[1], " line: ", line)
+					//		os.Exit(1)
+					//	}
+					//	if strings.Count(gitModuleAttributes, ",") > 2 {
+					//		log.Fatal("Error: Too many attributes in ", pf, " for module ", m[1], " line: ", line)
+					//		os.Exit(1)
+					//	}
+					//	if _, ok := puppetFile.gitModules[m[1]]; ok {
+					//		log.Fatal("Error: Duplicate module found in ", pf, " for module ", m[1], " line: ", line)
+					//		os.Exit(1)
+					//	}
+					//	if a := reModuleAttribute.FindStringSubmatch(gitModuleAttributes); len(a) > 1 {
+					//		gm := GitModule{}
+					//		//fmt.Println("found for git mod ", m[1], " attribute ", a[1], " with value ", a[2])
+					//		if a[1] == "git" {
+					//			gm.git = a[2]
+					//		} else if a[1] == "branch" {
+					//			gm.branch = a[2]
+					//		} else if a[1] == "tag" {
+					//			gm.tag = a[2]
+					//		} else if a[1] == "commit" {
+					//			gm.commit = a[2]
+					//		} else if a[1] == "ref" {
+					//			gm.ref = a[2]
+					//		}
+					//		if strings.Contains(gitModuleAttributes, ",") {
+					//			if a := reModuleAttribute.FindStringSubmatch(strings.SplitN(gitModuleAttributes, ",", 2)[1]); len(a) > 1 {
+					//				if a[1] == "git" {
+					//					gm.git = a[2]
+					//				} else if a[1] == "branch" {
+					//					gm.branch = a[2]
+					//				} else if a[1] == "tag" {
+					//					gm.tag = a[2]
+					//				} else if a[1] == "commit" {
+					//					gm.commit = a[2]
+					//				} else if a[1] == "ref" {
+					//					gm.ref = a[2]
+					//				}
+					//				//puppetFile.gitModules[m[1]] = GitModule{a[1]: a[2]}
+					//				//fmt.Println("found for git mod ", m[1], " attribute ", a[1], " with value ", a[2])
+					//			}
 
-						}
-						puppetFile.gitModules[m[1]] = gm
-					}
+					//		}
+					//		puppetFile.gitModules[m[1]] = gm
+					//	}
+					//	puppetFile.gitModules[m[1]] = GitModule{}
+					//	//fmt.Println("found git mod attribute ---> ", gitModuleAttributes)
 				}
 			}
 		}
